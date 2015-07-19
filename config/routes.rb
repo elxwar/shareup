95a58ffdecc07335ae0e2301ce3502aa6fcc6d19
Eric Waring <ericw@the-feel.co.uk>
2015-07-19 09:30:21 +0100

    added welcome and shares

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get '/dashboard' => 'welcome#dashboard'
+  root to: 'welcome#index'
+
+  namespace :api do
+    resources :shares
+  end
+
   devise_for :users,
              :controllers => {
                  :omniauth_callbacks => "users/omniauth_callbacks"
              }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
