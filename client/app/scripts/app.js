bda84ef2f90804c3656fb48a651ce774f03b0336
Eric Waring <ericw@the-feel.co.uk>
2015-07-19 11:08:31 +0100

    initial angular project - unlinked

---
+++ client/app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name shareupApp
+ * @description
+ * # shareupApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('shareupApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shareupApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shareupApp
+ */
+angular.module('shareupApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
